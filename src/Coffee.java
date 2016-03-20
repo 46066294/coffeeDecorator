f6535d43644f8d52ac93bd6ce3a1becfcacb0c68
46066294 <46066294p@iespoblenou.org>
2016-03-20 20:33:09 +0100

    Initial commit

---
+++ src/Coffee.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Mat on 20/03/2016.
+ */
+public class Coffee extends Beverage{
+    private String description;
+
+    public Coffee(){
+        this.description = "Cafe solo";
+    }
+    
+}
---
+++ src/DesCoffee.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Mat on 20/03/2016.
+ */
+public class DesCoffee {
+}
